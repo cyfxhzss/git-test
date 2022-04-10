d58ecb34325a0f7f5223a3be7e14340e49d3023d
CaoYifan <2938957008@qq.com>
2022-04-10 20:14:23 +0300

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Cao Yifan
+ * @date 2022/4/10 - 20:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
